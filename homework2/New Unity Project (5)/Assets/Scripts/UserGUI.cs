cb71457cc52cf8abd000d444043b42b27dd5c39d
BETARUN <594447215@qq.com>
2019-09-21 20:03:37 +0800

    homework2

---
+++ homework2/New Unity Project (5)/Assets/Scripts/UserGUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Com.Mygame;
+
+public class UserGUI : MonoBehaviour {
+	private UserAction action;
+	public int status = 0;
+	GUIStyle style;
+	GUIStyle buttonStyle;
+
+	void Start() {
+		action = Director.getInstance ().currentSceneController as UserAction;
+
+		style = new GUIStyle();
+		style.fontSize = 40;
+		style.alignment = TextAnchor.MiddleCenter;
+
+		buttonStyle = new GUIStyle("button");
+		buttonStyle.fontSize = 30;
+	}
+	void OnGUI() {
+		if (status == 1) {
+			GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-85, 100, 50), "游戏失败!", style);
+			if (GUI.Button(new Rect(Screen.width/2-70, Screen.height/2, 140, 70), "再玩一次", buttonStyle)) {
+				status = 0;
+				action.restart ();
+			}
+		} else if(status == 2) {
+			GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-85, 100, 50), "你胜利了!", style);
+			if (GUI.Button(new Rect(Screen.width/2-70, Screen.height/2, 140, 70), "再玩一次", buttonStyle)) {
+				status = 0;
+				action.restart ();
+			}
+		}
+	}
+}
