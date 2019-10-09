715808fa8eab950fbec32a7a1a86292e1f5b3d83
BETARUN <594447215@qq.com>
2019-10-09 21:40:48 +0800

    homework4

---
+++ homework4/New Unity Project (8)/Assets/Script/UserGUI.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UserGUI : MonoBehaviour {
+    private IUserAction action;
+    bool isFirst = true;
+    GUIStyle red;
+    GUIStyle black;
+
+    // Use this for initialization
+    void Start () {
+        action = Director.getInstance().current as IUserAction;
+        black = new GUIStyle("button");
+        black.fontSize = 20;
+        red = new GUIStyle();
+        red.fontSize = 30;
+        red.fontStyle = FontStyle.Bold;
+        red.normal.textColor = Color.red;
+        red.alignment = TextAnchor.UpperCenter;
+    }
+
+    private void OnGUI()
+    {
+        if (action.getGameState() == GameState.FUNISH)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 150, 200, 100), action.getScore() >= 30 ? "你胜利了" : "你失败了", red);
+            if(GUI.Button(new Rect(Screen.width / 2 - 60, Screen.height / 2 - 50, 120, 40), "重新开始", black))
+            {
+                SceneManager.LoadScene("DiskAttack");
+            }
+            return;
+        }
+        Rect rect = new Rect(Screen.width / 2 - 100, 0, 200, 40);
+        Rect rect2 = new Rect(Screen.width / 2 - 45, 60, 120, 40);
+
+        if (Input.GetButtonDown("Fire1") && action.getGameState() != GameState.PAUSE)
+        {
+            Vector3 pos = Input.mousePosition;
+            action.hit(pos);
+        }
+
+        if (!isFirst)
+        {
+            GUI.Label(rect, "你的分数: " + action.getScore().ToString(), red);
+        }
+        else
+        {
+            GUIStyle blackLabel = new GUIStyle();
+            blackLabel.fontSize = 16;
+            blackLabel.normal.textColor = Color.black;
+            GUI.Label(new Rect(Screen.width / 2 - 250, 120, 500, 200), "一共3个关卡，每一个关卡有10个飞碟，飞碟" +
+                "的颜色是不一样的\n如果攻击白色的飞碟，你会得到1分。如果攻击灰色的飞碟\n" +
+                "则会得到2分，如果攻击黑色的飞碟（速度最快），你会得到4分。\n" +
+                "游戏结束时得到超过30分就胜利了！", blackLabel);
+        }
+
+        if (action.getGameState() == GameState.RUNNING && GUI.Button(rect2, "暂停", black))
+        {
+            action.setGameState(GameState.PAUSE);
+        }
+        else if(action.getGameState() == GameState.PAUSE && GUI.Button(rect2, "继续", black))
+        {
+            action.setGameState(GameState.RUNNING);
+        }
+
+        if (isFirst && GUI.Button(rect2, "开始", black))
+        {
+            isFirst = false;
+            action.setGameState(GameState.ROUND_START);
+        }
+
+        if(!isFirst && action.getGameState() == GameState.ROUND_FINISH && GUI.Button(rect2, "下一关卡", black))
+        {
+            action.setGameState(GameState.ROUND_START);
+        }
+    }
+}
---
+++ homework4/readme.txt
@@ -0,0 +1,2 @@
+演示视频链接：
+https://v.youku.com/v_show/id_XNDM5MTkzNjE5Ng==.html?spm=a2hzp.8244740.0.0
