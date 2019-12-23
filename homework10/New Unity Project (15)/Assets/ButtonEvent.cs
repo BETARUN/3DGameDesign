5d079778b318f3420592cf7b29a32e955be0ad46
BETARUN <594447215@qq.com>
2019-12-23 18:18:13 +0800

    homework10

--- homework10/New Unity Project (15)/Assets/ButtonEvent.cs
+++ homework10/New Unity Project (15)/Assets/ButtonEvent.cs
@@ -6,54 +6,47 @@ using Vuforia;
 
 public class ButtonEvent : MonoBehaviour, IVirtualButtonEventHandler
 {
-    public VirtualButtonBehaviour[] vbs;
-    public GameObject cube;
-    public int index;
-
-    void Start()
-    {
-        vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
-        for (int i = 0; i < vbs.Length; i++)
-        {
-            vbs[i].RegisterEventHandler(this);
-        }
-        index = 0;
-        cube = GameObject.Find("ImageTarget/Cube");
-    }
+    public int count;
+    public GameObject objecta;
+    public VirtualButtonBehaviour[] vbutton;
 
     void Update()
     {
-
-    }
-
-    public void OnButtonPressed(VirtualButtonBehaviour vb)
-    {
-        index++;
-        if (index == 4)
-            index = 0;
-        switch (index)
-        {
-            case 0:
-                cube.GetComponent<Renderer>().material.color = Color.white;
-                break;
-            case 1:
-                cube.GetComponent<Renderer>().material.color = Color.red;
-                break;
-            case 2:
-                cube.GetComponent<Renderer>().material.color = Color.yellow;
-                break;
-            case 3:
-                cube.GetComponent<Renderer>().material.color = Color.green;
-                break;
-            case 4:
-                cube.GetComponent<Renderer>().material.color = Color.blue;
-                break;
-        }
-
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
+    }
+
+    void Start()
+    {
+        count = 0;
+        vbutton = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbutton.Length; i++)
+        {
+            vbutton[i].RegisterEventHandler(this);
+        }
+        objecta = GameObject.Find("ImageTarget/Cube");
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        count++;
+        switch (count)
+        {
+            case 0:
+                objecta.GetComponent<Renderer>().material.color = Color.white;
+                break;
+            case 1:
+                objecta.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case 2:
+                objecta.GetComponent<Renderer>().material.color = Color.yellow;
+                break;
+            case 3:
+                objecta.GetComponent<Renderer>().material.color = Color.green;
+                break;
+        }
 
     }
 
